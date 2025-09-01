8df0daeae0a40f6482bc757fcdea43176f56476f
Rodrigo Cerqueira <rodrigocerqueirareis@gmail.com>
2025-09-01 10:41:02 -0300

    refactor: replace footer text links with icons

--- myWebSite/src/App.jsx
+++ myWebSite/src/App.jsx
@@ -7,7 +7,6 @@ import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
